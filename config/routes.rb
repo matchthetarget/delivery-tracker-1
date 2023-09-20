6929097d50a6b6cc13562e54e4f0e27d5bcbc466
Ben Purinton <ben.purinton@gmail.com>
2023-09-20 13:59:39 -0800

    update from old target

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "deliveries#index"
+
+  # Routes for the Delivery resource:
+
+  # CREATE
+  post("/insert_delivery", { :controller => "deliveries", :action => "create" })
+          
+  # READ
+  get("/deliveries", { :controller => "deliveries", :action => "index" })
+  
+  # get("/deliveries/:path_id", { :controller => "deliveries", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_delivery/:path_id", { :controller => "deliveries", :action => "update" })
+  
+  # DELETE
+  get("/delete_delivery/:path_id", { :controller => "deliveries", :action => "destroy" })
+
+  #------------------------------
 end
